93f67591e53bf7578b1e98e5899517ac284efe70
José Lumbo <joselumbo77@gmail.com>
2022-05-11 19:28:04 -0300

    Crud atualizado

---
+++ src/main/java/com/generation/meublogpessoal1/Meublogpessoal1Application.java
@@ -0,0 +1,13 @@
+package com.generation.meublogpessoal1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Meublogpessoal1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Meublogpessoal1Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/generation/meublogpessoal1/security/UserDetailsImpl.java
@@ -0,0 +1,82 @@
+package com.generation.meublogpessoal1.security;
+
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.generation.meublogpessoal1.model.Usuario;
+
+public class UserDetailsImpl implements UserDetails {
+	private static final long serialVersionUID = 1L;
+
+	private String userName;
+	private String password;
+	private List<GrantedAuthority> authorities;
+
+	public UserDetailsImpl(Usuario usuario) {
+		this.userName = usuario.getUsuario();
+		this.password = usuario.getSenha();
+	}
+
+	public UserDetailsImpl() {
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return authorities;
+	}
+
+	@Override
+	public String getPassword() {
+		return password;
+	}
+
+	@Override
+	public String getUsername() {
+
+		return userName;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public static long getSerialversionuid() {
+		return serialVersionUID;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public void setAuthorities(List<GrantedAuthority> authorities) {
+		this.authorities = authorities;
+	}
+}
